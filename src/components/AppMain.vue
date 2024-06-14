4690db3f1aca0fc9d7510e8856b064d90823999e
MirkoValle <mirko.valle20@gmail.com>
2024-06-14 15:09:47 +0200

    Added ratinng with stars

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -45,7 +45,10 @@ export default{
             <li> {{film.original_title}} </li>
             <li> {{ film.original_language }} </li>
             <img :src="'https://flagsapi.com/' + film.original_language.toUpperCase() + '/flat/32.png'">
-            <li> {{ film.vote_average }} </li>
+            <div>
+                <i v-for="star in (Math.floor(film.vote_average / 2))" class="fa-solid fa-star"></i>
+                <i v-for="star in (5 - Math.floor(film.vote_average / 2))" class="fa-regular fa-star"></i>
+            </div>
         </ul>
         <ul v-for="(tvSerie, index) in store.tvSeries">
             <img :src="'https://image.tmdb.org/t/p/w154' + tvSerie.poster_path" alt="">
@@ -53,8 +56,10 @@ export default{
             <li> {{ tvSerie.original_name}} </li>
             <li> {{ tvSerie.original_language }} </li>
             <img :src="'https://flagsapi.com/' + tvSerie.original_language.toUpperCase() + '/flat/32.png'">
-            
-            <li> {{ tvSerie.vote_average }} </li>
+            <div>
+                <i v-for="star in (Math.floor(tvSerie.vote_average / 2))" class="fa-solid fa-star"></i>
+                <i v-for="star in (5 - Math.floor(tvSerie.vote_average / 2))" class="fa-regular fa-star"></i>
+            </div>
         </ul>
     </section>
 </template>
